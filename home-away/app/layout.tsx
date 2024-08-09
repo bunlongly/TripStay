30b62decde8b614499644826fae37efe60b9e563
Bunlong <bunlongly03@gmail.com>
2024-08-09 14:55:55 +0700

    navbar structure

--- home-away/app/layout.tsx
+++ home-away/app/layout.tsx
@@ -1,22 +1,26 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Navbar from '@/components/ui/navbar/Navbar';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "HomeAway",
-  description: "Feel at home, away from home",
+  title: 'HomeAway',
+  description: 'Feel at home, away from home'
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={inter.className}>
+        <Navbar/>
+        <main className='container py-10'>{children}</main>
+      </body>
     </html>
   );
 }
--- home-away/components/ui/navbar/Navbar.tsx
+++ home-away/components/ui/navbar/Navbar.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Logo from './Logo';
+import NavSearch from './NavSearch';
+import LinksDropdown from './LinksDropdown';
+import DarkMode from './DarkMode';
 
 function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
-}
+    return (
+      <nav className='border-b'>
+        <div className='container flex flex-col sm:flex-row  sm:justify-between sm:items-center flex-wrap gap-4 py-8'>
+          <Logo />
+          <NavSearch />
+          <div className='flex gap-4 items-center '>
+            <DarkMode />
+            <LinksDropdown />
+          </div>
+        </div>
+      </nav>
+    );
+  }
 
-export default Navbar
+export default Navbar;
