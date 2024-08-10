7ab11f59339292a02644420cef04756f2529fa92
Bunlong <bunlongly03@gmail.com>
2024-08-10 14:38:56 +0700

    clerk setup

---
+++ home-away/app/favorites/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Favorties() {
+  return (
+    <div>Favorties</div>
+  )
+}
+
+export default Favorties
---
+++ home-away/app/rentals/create/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CreateRentals() {
+  return (
+    <div>CreateRentals</div>
+  )
+}
+
+export default CreateRentals
