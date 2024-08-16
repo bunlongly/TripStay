c47c8d3e58e5480e7fa0ae52b9b69030ac0f3881
Bunlong <bunlongly03@gmail.com>
2024-08-16 10:36:58 +0700

    fetch user profile

--- home-away/utils/actions.ts
+++ home-away/utils/actions.ts
@@ -6,6 +6,15 @@ import { auth, clerkClient, currentUser } from '@clerk/nextjs/server';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error('You must be logged in to access this route');
+  }
+  if (!user.privateMetadata.hasProfile) redirect('/profile/create');
+  return user;
+};
+
 export const createProfileAction = async (
   prevState: any,
   formData: FormData
@@ -22,33 +31,52 @@ export const createProfileAction = async (
         clerkId: user.id,
         email: user.emailAddresses[0].emailAddress,
         profileImage: user.imageUrl ?? '',
-        ...validatedFields,
-      },
+        ...validatedFields
+      }
     });
     await clerkClient.users.updateUserMetadata(user.id, {
       privateMetadata: {
-        hasProfile: true,
-      },
+        hasProfile: true
+      }
     });
   } catch (error) {
     return {
-      message: error instanceof Error ? error.message : 'An error occurred',
+      message: error instanceof Error ? error.message : 'An error occurred'
     };
   }
   redirect('/');
 };
 
 export const fetchProfileImage = async () => {
-  const user = await currentUser()
-  if(!user) return null
+  const user = await currentUser();
+  if (!user) return null;
 
   const profile = await db.profile.findUnique({
-    where:{
+    where: {
       clerkId: user.id
     },
     select: {
-      profileImage:true,
-    },
-  })
-  return profile?.profileImage
-}
+      profileImage: true
+    }
+  });
+  return profile?.profileImage;
+};
+
+export const fetchProfile = async () => {
+  const user = await getAuthUser();
+
+  const profile = await db.profile.findUnique({
+    where: {
+      clerkId: user.id
+    }
+  });
+  if (!profile) return redirect('/profile/create');
+  return profile;
+};
+
+export const updateProfileAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  return { message: 'update profile action' };
+};
