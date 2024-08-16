bfa98aa27ab09b4c0629925142729798e52712bf
Bunlong <bunlongly03@gmail.com>
2024-08-16 10:08:34 +0700

    fetch profile image action

--- home-away/components/navbar/UserIcon.tsx
+++ home-away/components/navbar/UserIcon.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
 import { LuUser2 } from 'react-icons/lu';
+import { fetchProfileImage } from '@/utils/actions';
 
-function UserIcon() {
-  return (
-    <LuUser2 className='w-6 h-6 bg-primary rounded text-white'>
-      UserIcon
-    </LuUser2>
-  );
+async function UserIcon() {
+  const profileImage = await fetchProfileImage();
+
+  if (profileImage)
+    return (
+      <img src={profileImage} className='w-6 h-6 rounded-full object-cover' />
+    );
+  return <LuUser2 className='w-6 h-6 bg-primary rounded-full text-white' />;
 }
-
 export default UserIcon;
