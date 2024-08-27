582c195ad37a51b985b704c4c91e8d0077ca1e4b
Bunlong <bunlongly03@gmail.com>
2024-08-27 19:52:18 +0700

    loading cards

--- home-away/app/page.tsx
+++ home-away/app/page.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import PropertiesContainer from '@/components/home/PropertiesContainer';
 import CategoriesList from '@/components/home/CategoriesList';
+import { Suspense } from 'react';
+import LoadingCards from '@/components/card/LoadingCards';
 
 function HomePage({
   searchParams
 }: {
   searchParams: { category?: string; search?: string };
 }) {
-  console.log(searchParams)
+  console.log(searchParams);
   return (
     <section>
-    <CategoriesList
-      category={searchParams?.category}
-      search={searchParams?.search}
-    />
-    <PropertiesContainer
-      category={searchParams?.category}
-      search={searchParams?.search}
-    />
-  </section>
+      <CategoriesList
+        category={searchParams?.category}
+        search={searchParams?.search}
+      />
+      <Suspense fallback={<LoadingCards />}>
+        <PropertiesContainer
+          category={searchParams?.category}
+          search={searchParams?.search}
+        />
+      </Suspense>
+    </section>
   );
 }
 
