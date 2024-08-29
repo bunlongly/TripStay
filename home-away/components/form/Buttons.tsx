e0c6377ffb147ff05cf1cbfd8507b6a6462223bd
Bunlong <bunlongly03@gmail.com>
2024-08-29 15:21:58 +0700

    favorites signIn button

--- home-away/components/form/Buttons.tsx
+++ home-away/components/form/Buttons.tsx
@@ -2,6 +2,10 @@
 import { ReloadIcon } from '@radix-ui/react-icons';
 import { useFormStatus } from 'react-dom';
 import { Button } from '@/components/ui/button';
+import { SignInButton } from '@clerk/nextjs';
+import { FaRegHeart, FaHeart } from 'react-icons/fa';
+
+
 
 type btnSize = 'default' | 'lg' | 'sm';
 
@@ -35,3 +39,20 @@ export function SubmitButton({
     </Button>
   );
 }
+
+
+export const CardSignInButton = () => {
+  return (
+    <SignInButton mode='modal'>
+      <Button
+        type='button'
+        size='icon'
+        variant='outline'
+        className='p-2 cursor-pointer'
+        asChild
+      >
+        <FaRegHeart />
+      </Button>
+    </SignInButton>
+  );
+};
