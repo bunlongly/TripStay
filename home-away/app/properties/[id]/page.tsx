a14d7e94f6c02da4be8e0e6382f8b6255feb3c61
Bunlong <bunlongly03@gmail.com>
2024-08-30 11:11:12 +0700

    breadcrumbs component

--- home-away/app/properties/[id]/page.tsx
+++ home-away/app/properties/[id]/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import FavoriteToggleButton from '@/components/card/FavoriteToggleButton';
 import { fetchPropertyDetails } from '@/utils/actions';
+import BreadCrumbs from '@/components/properties/BreadCrumbs';
 
 import { redirect } from 'next/navigation';
 
@@ -8,6 +10,17 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
   if (!property) redirect('/');
   const { baths, bedrooms, beds, guests } = property;
   const details = { baths, bedrooms, beds, guests };
-  return <div>PropertyDetailsPage</div>;
+  return (
+    <section>
+      <BreadCrumbs name={property.name} />
+      <header className='flex justify-between items-center mt-4'>
+        <h1 className='text-4xl font-bold '>{property.tagline}</h1>
+        <div className='flex items-center gap-x-4'>
+          {/* share button */}
+          <FavoriteToggleButton propertyId={property.id} />
+        </div>
+      </header>
+    </section>
+  );
 }
 export default PropertyDetailsPage;
