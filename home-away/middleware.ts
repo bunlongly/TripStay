35fad083189b5248767c0e3a875bc614343447ab
Bunlong <bunlongly03@gmail.com>
2024-09-07 11:50:14 +0700

    update middleware

--- home-away/middleware.ts
+++ home-away/middleware.ts
@@ -1,9 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-
 import { NextResponse } from 'next/server';
-
 const isPublicRoute = createRouteMatcher(['/', '/properties(.*)']);
-
 const isAdminRoute = createRouteMatcher(['/admin(.*)']);
 export default clerkMiddleware(async (auth, req) => {
   const isAdminUser = auth().userId === process.env.ADMIN_USER_ID;
@@ -12,8 +9,6 @@ export default clerkMiddleware(async (auth, req) => {
   }
   if (!isPublicRoute(req)) auth().protect();
 });
-
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)']
 };
-
